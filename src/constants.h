3ba4a1c6ab82ad0a8947a265c80e1e1cbfbc8138
Adam Lanzafame <sysal@tuta.io>
2026-01-10 23:04:42 +1030

    Updated constants.h
    Added Emoji credit constant.
    
    Updated to remove redundant specifiers.

--- src/constants.h
+++ src/constants.h
@@ -21,29 +21,31 @@
 #pragma once
 
 namespace Passwords {
-inline constexpr const char* Name           = "Passwords";
-inline constexpr const char* Organization   = "sysal1280";
-inline constexpr const char* Version        = "1.3.1";
-inline constexpr const char* Icon           = ":/password.png";
+inline constexpr auto Name           = "Passwords";
+inline constexpr auto Organization   = "sysal1280";
+inline constexpr auto Version        = "1.3.1";
+inline constexpr auto Icon           = ":/password.png";
 
 inline constexpr int SBTransientMessageTime = 5000;
 
-inline constexpr const char* HelpBaseUrl    = "https://sysal1280.github.io/passwords/";
-inline constexpr const char* GitUrl         = "https://github.com/sysal1280/passwords.git";
-inline constexpr const char* BugUrl         = "https://github.com/sysal1280/passwords/security";
+inline constexpr auto HelpBaseUrl    = "https://sysal1280.github.io/passwords/";
+inline constexpr auto GitUrl         = "https://github.com/sysal1280/passwords.git";
+inline constexpr auto BugUrl         = "https://github.com/sysal1280/passwords/security";
 
-inline constexpr const char* License        = "Licensed under the <a href=\"https://www.gnu.org/licenses/gpl-3.0.html\">GNU GPLv3 License</a> or later.";
-inline constexpr const char* WarrantyDisclaimer =
+inline constexpr auto License        = "Licensed under the <a href=\"https://www.gnu.org/licenses/gpl-3.0.html\">GNU GPLv3 License</a> or later.";
+inline constexpr auto WarrantyDisclaimer =
     "This program is distributed in the hope that it will be useful, but it is provided without any warranty; "
     "including without the implied warranties of merchantability or fitness for a particular purpose. "
     "For full details, see the GNU General Public License.";
 
-inline constexpr const char* WordlistsCredit = "Wordlists by Sam Schlinkert (MIT License)";
-inline constexpr const char* MaterialSymbolsCredit = "Material Symbols by Google (Apache 2.0 License)";
-inline constexpr const char* IconCreditFormat = "%1 icon by Iconic Panda (Flaticon)";
+inline constexpr auto WordlistsCredit = "Wordlists by Sam Schlinkert (MIT License)";
+inline constexpr auto MaterialSymbolsCredit = "Material Symbols by Google (Apache 2.0 License)";
+inline constexpr auto IconCreditFormat = "%1 icon by Iconic Panda (Flaticon)";
+inline constexpr auto EmojiCredit = "Fluent Emoji by Microsoft, licensed under the MIT License.";
 
-inline constexpr const char* debugWarningSB = "Debug Version Only";
-inline constexpr const char* debugWarningMB =
+inline constexpr auto debugWarningSB = "Debug Version Only";
+
+inline constexpr auto debugWarningMB =
     "This is a Debug build.\n\n"
     "Debug builds include extra diagnostic information and reduced security "
     "protections. They are not safe for production use.\n\n"
