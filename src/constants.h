4eb00cfa8c8eff2b814b8a777fc10e530f96e937
Adam Lanzafame <sysal@tuta.io>
2026-01-11 11:47:51 +1030

    Github workflow improvement.

--- src/constants.h
+++ src/constants.h
@@ -23,7 +23,7 @@
 namespace Passwords {
 inline constexpr auto Name           = "Passwords";
 inline constexpr auto Organization   = "sysal1280";
-inline constexpr auto Version        = "1.3.9";
+inline constexpr auto Version        = "1.3.10";
 inline constexpr auto Icon           = ":/password.png";
 
 inline constexpr int SBTransientMessageTime = 5000;
